356e3731a049f4cd4c18d89f39d4860149af9909
Dana Carolina Moreno Jiménez <danammoreno28@gmail.com>
2024-10-29 10:00:20 -0600

    29/10/2024

--- App.js
+++ App.js
@@ -1,27 +1,99 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import {TextButton} from './components/TextButton';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [peso, setPeso] = useState(0);
+  const [estatura, setEstatura] = useState(0);
+  const [imc, setImc] = useState(null);
+  const [estadoNutricional, setEstadoNutricional] = useState(null);
+
+  function PesoInputHandler(enteredText) {
+    setPeso(enteredText); 
+  } 
+  
+    function EstaturaInputHandler(enteredText) {
+    setEstatura(enteredText); 
+  }
+
+  function IndiceDeMasaCorporal(peso, estatura) {
+    return peso / (estatura * estatura); 
+  }
+ 
+  function DeterminaEstadoNutricional(imc) {
+    if (imc < 18.5) {
+      return "Peso bajo";
+    }
+    if (imc < 25.0) {
+      return "Peso normal";
+    }
+    if (imc < 30.0) {
+      return "Sobrepeso";
+    }
+    if (imc < 40.0) {
+      return "Obesidad";
+    }
+    return "Obesidad Extrema"; 
+  }
+
+  function onCalcularButtonTapped(){
+    const bmi = IndiceDeMasaCorporal(peso, estatura);
+    setImc(bmi.toFixed(4));
+    setEstadoNutricional(DeterminaEstadoNutricional(bmi));
+  }
+
+  function onLimpiarButtonTapped() {
+    setPeso(0);
+    setEstatura(0);
+    setImc(null);
+    setEstadoNutricional(null);
+  }
+
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.inputContainer}>
         <TextInput 
         style={styles.textInput}
-        placeholder='Peso de la persona en kilogramos'/>
+        placeholder='Peso de la persona en kilogramos'
+        onChangeText={PesoInputHandler}
+        value={peso} />
         <TextInput 
         style={styles.textInput}
-        placeholder='Estatura de la persona en metros'/>
+        placeholder='Estatura de la persona en metros'
+        onChangeText={EstaturaInputHandler}
+        value={estatura} />
       </View>
 
       <View style={styles.commandsContainer}>
-        <Button title='Calcular' color={'green'}/>
-        <Button title='Limpiar' color={'red'}/>
-      </View>
+          <TextButton title= 'Calcular' onPress={onCalcularButtonTapped}/>
+          <TextButton title= 'Limpiar' onPress={onLimpiarButtonTapped}/>
+        {/*<View> 
+          <Pressable onPress={onCalcularButtonTapped}>
+          <View style={styles.buttonCalcular}>
+          <Text style={styles.textButton}>Calcular</Text>
+          </View>
+        </Pressable> 
+        </View> */}
+        {/* <View>
+        <Pressable onPress={onLimpiarButtonTapped}>
+          <View style={styles.buttonLimpiar}>
+          <Text style={styles.textButton}>Limpiar</Text>
+          </View>
+        </Pressable>
+        </View> */}
+        {/* <Button title='Calcular' color={'green'}
+        onPress={onCalcularButtonTapped} />
+        <Button title='Limpiar' color={'red'}
+        onPress={onLimpiarButtonTapped} /> */}
+        </View> 
       
       <View style={styles.resultsContainer}>
         <Text style={styles.resultText}>El índice de masa corporal es:</Text>
-        <Text style={styles.result}>24.376</Text>
+        <Text style={styles.result}>{imc}</Text>
         <Text style={styles.resultText}>El estado nutricional es:</Text>
-        <Text style={styles.result}>Peso normal</Text>
+        <Text style={styles.result}>{estadoNutricional}</Text>
       </View>
     </View>
   );
@@ -40,28 +112,25 @@ const styles = StyleSheet.create({
     borderColor: 'lightgray',
     borderWidth: 2,
     borderRadius: 8,
-    padding: 16,
-    
+    padding: 16
   },
   commandsContainer: {
     flexDirection: 'row',
     marginVertical: 24,
     justifyContent: 'center',
-    gap: 48,
-
+    gap: 48
   }, 
   resultsContainer: {
     borderColor: 'lightgray',
     borderWidth: 2,
     borderRadius: 8,
-    padding: 16,
-
+    padding: 16
   },
   textInput: {
     borderColor: 'lightgray',
     borderWidth: 1, 
     margin: 4,
-    fontSize: 18, 
+    fontSize: 18
   },
   resultText: {
     fontSize: 18
@@ -71,5 +140,24 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     marginVertical: 12
-  }
+  },
+  // buttonCalcular: {
+  //   borderColor: 'green',
+  //   backgroundColor: 'green',
+  //   borderWidth: 2, 
+  //   borderRadius: 5,
+  //   padding: 8
+  // },
+  // buttonLimpiar: {
+  //   borderColor: 'red',
+  //   backgroundColor: 'red',
+  //   borderWidth: 2, 
+  //   borderRadius: 5,
+  //   padding: 8
+  // },
+  // textButton: {
+  //   color: 'white',
+  //   fontSize: 18
+  // }
+
 });
